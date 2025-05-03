27c11282487f0bef2793a3b196fe64ff252385d5
coderspree19 <rudranarayansatapathy19@gmail.com>
2025-05-03 18:10:14 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+let name="rudra"//string
+let age=19//number
+let isloggedin=false//boolean
+//number 2^53
+//big int
+//string=""
+//boolean=true/false
+//null=standalone value
+//undefined
+//symbol=unique
+//object
