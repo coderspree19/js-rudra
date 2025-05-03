840def3d6135d383fa4f4f8ebb1086b18133c7a9
coderspree19 <rudranarayansatapathy19@gmail.com>
2025-05-03 17:38:14 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+//variable
+const accountId=14423
+let accountemail="rudra@gmail.com"
+var accountpassword="12345"//prefer not to use var because of issue of block scope and functional scope
+accountcity="tumkur"
+//accountId=2 //not allowed due to it is constant
+accountemail="hc.@gmail.com"
+accountpassword="2121"
+accountcity="bengaluru"
+let accountstate;
+console.log(accountId);
+console.table([accountId,accountemail,accountpassword,accountcity,accountstate])
