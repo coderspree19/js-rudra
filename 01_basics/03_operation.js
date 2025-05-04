503e6a5442e41c75da63e1b97e069d0af1176df4
coderspree19 <rudranarayansatapathy19@gmail.com>
2025-05-04 06:50:24 +0000

    basics of operation

---
+++ 01_basics/03_operation.js
@@ -0,0 +1,17 @@
+//operation
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2**2)
+console.log(2%2)
+//concat(add)
+//let str1="hello"
+//let str2="rudra"
+/*str3=str1+str2
+console.log(str3)*/
+console.log(1+"2")
+console.log("1"+2)
+console.log("1"+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
