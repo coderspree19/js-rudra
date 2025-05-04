ba3afcdaef5755fdfc4b96e90dab36b5bc58f9af
coderspree19 <rudranarayansatapathy19@gmail.com>
2025-05-04 08:25:38 +0000

    comparision

---
+++ 01_basics/04_compsarision.js
@@ -0,0 +1,13 @@
+/*console.log(2>1)
+console.log(2<1)
+console.log(2==1)
+console.log(2>=1)
+console.log(2<=1)
+console.log(2!=1)*/
+console.log("2">1)
+console.log("02">1)
+console.log("2"+1+1)
+console.log(1+2+"2")
+//datatype of two types primitive and non primitive
+//primitive 7 datatpes :string,number,boolean,null,undefined,symbol,Bigint
+//reference(nonprimitive):array,object,,function
